35700a9665bdd259d8dd29635f2227be09e39611
rakaldi10 <rakaldi10@gmail.com>
2022-07-19 10:26:45 +0700

    Tugas 1 PHP
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$m1 = "13000";
+$m2 = "15000";
+$m3 = "13000";
+$m4 = "1000";
+$m5 = "1000";
+$m6 = "3000";
+$m7 = "5000";
+$m8 = "4000";
+$m9 = "5000";
+echo ("<h1>Daftar Menu</h1>");
+printf("<div><ul>
+<li>Ayam Goreng =" . $m1 . " </li>
+<li>Ayam Bakar =" . $m2 . " </li>
+<li>Ayam Sayur =" . $m3 . " </li>
+<li>Tempe Goreng =" . $m4 . " </li> 
+<li>Tahu Goreng =" . $m5 . " </li>
+<li>Soto =" . $m6 . " </li>
+<li>Nasi Putih =" . $m7 . " </li>
+<li>Es Teh =" . $m8 . " </li>
+<li>Es Tebu =" . $m9 . " </li>   
+</ul></div>");
